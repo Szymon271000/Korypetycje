56e2a21db6fb51c1628d39b059345608f333aa95
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-08-30 12:52:14 +0200

    Add files via upload

--- Wczytywanie_z_warunkami/Wczytywanie_z_warunkami/Program.cs
+++ Wczytywanie_z_warunkami/Wczytywanie_z_warunkami/Program.cs
@@ -6,12 +6,12 @@ namespace Wczytywanie_z_warunkami
     {
         static void Main(string[] args)
         {
-            double positive = double.PositiveInfinity;
             int first_min = -11;
+            int second_min = 55;
             Console.WriteLine("Podaj liczbe:");
             int n = int.Parse(Console.ReadLine());
             int counter = 0;
-            while ((n <= positive) && ( n <= first_min))
+            while ((n <= first_min) || ( n >= first_min))
             {
                 Console.WriteLine("Podaj liczbe:");
                 n = int.Parse(Console.ReadLine());
