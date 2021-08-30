a668c173fb147fd4f21ea9269fa9abbc94e4aa62
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-08-30 10:11:07 +0200

    Add files via upload

---
+++ Zero-Jeden/Zero-Jeden/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Zero_Jeden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("1");
+                }
+                else
+                {
+                    Console.Write("0");
+                }
+            }
+        }
+    }
+}
