2aa6a4750f94c8985168ce410a4f9e9c12668c64
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-08-30 11:39:42 +0200

    Add files via upload

---
+++ Wczytywanie_z_warunkami/Wczytywanie_z_warunkami/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Wczytywanie_z_warunkami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double positive = double.PositiveInfinity;
+            int first_min = -11;
+            Console.WriteLine("Podaj liczbe:");
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+            while ((n <= positive) && ( n <= first_min))
+            {
+                Console.WriteLine("Podaj liczbe:");
+                n = int.Parse(Console.ReadLine());
+                if (counter == 3)
+                {
+                    break;
+                }
+                counter++;
+            }
+            Console.WriteLine("Koniec programu");
+        }
+    }
+}
