a760adabbb97e81c7960bd362b1bc2aee00da684
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-08-31 16:12:34 +0200

    Add files via upload

---
+++ Kamien,_papier_i_nozyce/Kamien,_papier_i_nozyce/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Kamien__papier_i_nozyce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string[] options;
+                options = new string[] { "Rock", "Paper", "Scissors" };
+                for (int i = 0; i < options.Length; i++)
+                {
+                    Console.Write($"{i} {options[i]} ");
+                }
+                Console.WriteLine();
+                int user_option = int.Parse(Console.ReadLine());
+                Random rnd = new Random();
+                int index = rnd.Next(options.Length);
+                Console.WriteLine($"Komputer wybral: {options[index]}");
+                if (user_option == 0 && index == 0 || user_option == 1 && index == 1 || user_option == 2 && index == 2)
+                {
+                    Console.WriteLine("Tie");
+                }
+                else if (user_option == 0 && index == 2 || user_option == 1 && index == 0 || user_option == 2 && index == 1)
+                {
+                    Console.WriteLine("Won");
+                }
+                else
+                {
+                    Console.WriteLine("Lost");
+                    Console.WriteLine("End of programm");
+                    break;
+                }
+            }    
+        }
+    }
+}
