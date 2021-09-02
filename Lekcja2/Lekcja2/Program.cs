421147730a412c482a7f6f4ce28d93d14fded3ec
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-02 18:46:13 +0200

    Lekcja 2

---
+++ Lekcja2/Lekcja2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lekcja2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 0;
+
+            if(b == 100 && (a == 8 || a == 5))
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("Błąd!");
+            }
+        }
+    }
+}
