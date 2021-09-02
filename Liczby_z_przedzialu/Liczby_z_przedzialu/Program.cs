b33ef2a169c54a6792cf6b9f380e73910d0cc5d7
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-02 11:20:04 +0200

    Add files via upload

---
+++ Liczby_z_przedzialu/Liczby_z_przedzialu/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Liczby_z_przedzialu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj poczatek przedzialu: ");
+            int i = int.Parse(Console.ReadLine());
+            Console.Write("Podaj koniec przedzialu: ");
+            int n = int.Parse(Console.ReadLine());
+            while (i <= n)
+            {
+                Console.Write(i + " ");
+                i++;
+            }
+        }
+    }
+}
