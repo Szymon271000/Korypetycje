e9fd653bb1637e70c042b65121759dba73bad90c
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-02 13:21:17 +0200

    Add files via upload

---
+++ Zgaduj_zgadula/Zgaduj_zgadula/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Zgaduj_zgadula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int random_numer = rnd.Next(0, 10);
+            Console.Write("Podaj liczbe: ");
+            int n = int.Parse(Console.ReadLine());
+            while (n != random_numer)
+            {
+                if (n > random_numer)
+                {
+                    Console.WriteLine("Za duza");
+                }
+                else
+                {
+                    Console.WriteLine("Za mala");
+                }
+                Console.Write("Podaj liczbe: ");
+                n = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Wygrana, koniec programu.");
+        }
+    }
+}
