a295487814371a1a67249072d862ca546c623324
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-02 08:43:14 +0200

    Add files via upload

---
+++ Mnozenia/Mnozenia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Mnozenia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Liczba: ");
+            int n = int.Parse(Console.ReadLine());
+            int i = 1;
+            while (i < 11)
+            {
+                int product = n * i;
+                Console.WriteLine($"{n} * {i} = {product}");
+                i++;
+            }
+        }
+    }
+}
