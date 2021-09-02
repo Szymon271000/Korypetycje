77f340562f11fbc4d2aa0bed05d1d4fa792b20e2
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-02 08:29:38 +0200

    Add files via upload

---
+++ Petla_liczbowa/Petla_liczbowa/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Petla_liczbowa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj wartosc: ");
+            int n = int.Parse(Console.ReadLine());
+            int i = 0;
+            while (i < n + 1)
+            {
+                Console.Write(i + " ");
+                i++;
+            }
+        }
+    }
+}
