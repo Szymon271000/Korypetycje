d2aa116669c18132a112b88e7b92e46bf7eefc0c
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-02 11:33:37 +0200

    Add files via upload

---
+++ Kwadraty_liczb/Kwadraty_liczb/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Kwadraty_liczb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj liczbe: ");
+            int n = int.Parse(Console.ReadLine());
+            int i = 1;
+            while (i <= n)
+            {
+                double i_quadrat = Math.Pow(i,2);
+                Console.Write(i_quadrat + " ");
+                i++;
+            }
+        }
+    }
+}
