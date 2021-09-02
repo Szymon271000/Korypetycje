10d1bd62edd809bbf5f3647bf2d87857f96e1f2f
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-02 15:09:01 +0200

    Add files via upload

---
+++ Min_i_max/Min_i_max/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Min_i_max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write("Podaj liczbe: ");
+                int n = int.Parse(Console.ReadLine());
+                if (n < min)
+                {
+                    min = n;
+                }
+                if (n > max)
+                {
+                    max = n;
+                }
+            }
+            Console.WriteLine($"Min: {min}");
+            Console.WriteLine($"Max: {max}");
+        }
+    }
+}
