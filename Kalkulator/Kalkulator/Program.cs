202f368f76e11c4fadc7afb05ef693e951108574
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-02 17:32:26 +0200

    Add files via upload

---
+++ Kalkulator/Kalkulator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entry = "Wybierz operacje: 0. Wyjscie. 1. Dodawanie 2.Odejmowanie 3. Mnozenie 4. Dzielenie";
+            Console.WriteLine(entry);
+            string option = Console.ReadLine();
+            while (option != "0")
+            {
+                if (option == "1")
+                {
+                    Console.Write("Podaj liczbe: ");
+                    int first_number = int.Parse(Console.ReadLine());
+                    Console.Write("Podaj liczbe: ");
+                    int second_number = int.Parse(Console.ReadLine());
+                    int sum = first_number + second_number;
+                    Console.WriteLine($"Wynik: {sum}");
+                }
+                else if (option == "2")
+                {
+                    Console.Write("Podaj liczbe: ");
+                    int first_number = int.Parse(Console.ReadLine());
+                    Console.Write("Podaj liczbe: ");
+                    int second_number = int.Parse(Console.ReadLine());
+                    int difference = first_number - second_number;
+                    Console.WriteLine($"Wynik: {difference}");
+                }
+                else if (option == "3")
+                {
+                    Console.Write("Podaj liczbe: ");
+                    int first_number = int.Parse(Console.ReadLine());
+                    Console.Write("Podaj liczbe: ");
+                    int second_number = int.Parse(Console.ReadLine());
+                    int product = first_number * second_number;
+                    Console.WriteLine($"Wynik: {product}");
+                }
+                else if(option == "4")
+                {
+                    Console.Write("Podaj liczbe: ");
+                    int first_number = int.Parse(Console.ReadLine());
+                    Console.Write("Podaj liczbe: ");
+                    int second_number = int.Parse(Console.ReadLine());
+                    int quotient = first_number / second_number;
+                    Console.WriteLine($"Wynik: {quotient}");
+                }
+                Console.WriteLine(entry);
+                option = Console.ReadLine();
+            }
+            Console.WriteLine("Wyjscie! Koniec programu");
+        }
+    }
+}
