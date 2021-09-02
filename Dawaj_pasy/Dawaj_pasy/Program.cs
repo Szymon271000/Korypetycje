a57267a49400a0d397351cdc443f41ce3d916ee7
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-02 13:10:23 +0200

    Add files via upload

---
+++ Dawaj_pasy/Dawaj_pasy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Dawaj_pasy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string right_passwrod = "1234";
+            Console.Write("Podaj haslo: ");
+            string prove = Console.ReadLine();
+            if (prove != right_passwrod)
+            {
+                Console.WriteLine("Niepoprawne!");
+            }
+            while (prove != right_passwrod)
+            {
+                Console.Write("Podaj haslo: ");
+                prove = Console.ReadLine();
+                if (prove != right_passwrod)
+                {
+                    Console.WriteLine("Niepoprawne!");
+                }
+            }
+            Console.WriteLine("Ok, zalogowano do systemu");
+        }
+    }
+}
