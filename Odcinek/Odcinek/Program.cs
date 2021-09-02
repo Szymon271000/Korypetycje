17888dc9f14f51c79158732d73d44867d352c7e6
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-02 11:12:46 +0200

    Add files via upload

---
+++ Odcinek/Odcinek/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Odcinek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj dlugosc: ");
+            int n = int.Parse(Console.ReadLine());
+            int i = 0;
+            while (i < n)
+            {
+                Console.Write("-");
+                i++;
+            }
+        }
+    }
+}
