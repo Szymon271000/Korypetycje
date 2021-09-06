b431b055dd1df67a251f74aa8d6f32780e6f5769
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-06 21:10:06 +0200

    Zamiana_indeksow

---
+++ Zamiana_indeksow/Zamiana_indeksow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Zamiana_indeksow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = new int[10];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Random rnd = new Random();
+                int number = rnd.Next(1, 11);
+                numbers[i] = number;
+                Console.WriteLine(numbers[i]);
+            }
+            Console.WriteLine("Podaj indeksy ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var temp = numbers[a];
+                numbers[a] = numbers[b];
+                numbers[b] = temp;
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
