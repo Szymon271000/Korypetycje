a16157be04efd07eec5afbe1585ca121585b937a
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-06 20:41:53 +0200

    Liczby_w_tablicy

---
+++ Liczby_w_tablicy/Liczby_w_tablicy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Liczby_w_tablicy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[11];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                numbers[i] = i;
+                Console.WriteLine(numbers[i]);
+            }
+            
+
+        }
+    }
+}
