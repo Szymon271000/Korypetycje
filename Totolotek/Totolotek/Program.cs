196137a08e630cf5ee9a48848edc4d409a6f38c9
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-06 21:17:05 +0200

    Suma_tablicy

---
+++ Totolotek/Totolotek/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Totolotek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[6];
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Random rnd = new Random();
+                int number = rnd.Next(1, 100);
+                numbers[i] = number;
+                sum = sum + numbers[i];
+                Console.WriteLine(numbers[i]);
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
