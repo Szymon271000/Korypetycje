7d2c8c0c8676b63055c29895f1ea72f306f25021
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-06 19:51:33 +0200

    Update Program.cs
    Leckja 2

--- Lekcja2/Lekcja2/Program.cs
+++ Lekcja2/Lekcja2/Program.cs
@@ -6,7 +6,7 @@ namespace Lekcja2
     {
         static void Main(string[] args)
         {
-            int a = 5;
+            /*int a = 5;
             int b = 0;
 
             if(b == 100 && (a == 8 || a == 5))
@@ -18,7 +18,61 @@ namespace Lekcja2
                 Console.WriteLine("Błąd!");
             }
 
-            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");*/
+
+            int liczba = 1254;
+            int sum = 0;
+            while(liczba != 0)
+            {
+                int module = liczba % 10;
+                int quotient = liczba / 10;
+                sum = sum + module;
+                liczba = quotient;
+            }
+            Console.WriteLine(sum);
+
+            int[] numbers = new int[10];
+            //indeksy sa od 0 do rozmiar tablicy - 1
+            numbers[4] = 71;
+            numbers[5] = 6;
+            numbers[7] = 15;
+            numbers[8] = 111;
+            numbers[2] = 18;
+            Console.WriteLine(numbers);
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < numbers.Length / 2; i++)
+            {
+                Console.WriteLine(numbers[i * 2]);
+            }
+
+            //Console.Write("Podaj index: ");
+            //int index = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Wartosc pod indeksem {index} : {numbers[index]}");
+            Random rnd = new Random();
+            for (int i = 0; i < numbers.Length; i += 2 )
+            {
+                int one = 1;
+                numbers[i] = one;
+            }
+
+            //numbers[0] = 1;
+            //numbers[1] = 1;
+            //numbers[2] = 1;
+            //numbers[3] = 1;
+            //numbers[4] = 1;
+            //numbers[5] = 1;
+            //numbers[6] = 1;
+            //numbers[7] = 1;
+            //numbers[8] = 1;
+            //numbers[9] = 1;
+
+
         }
     }
 }
