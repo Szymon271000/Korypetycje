9639566f6f0bfe06f76b8ae12b937ae542b4cdc0
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-06 22:16:03 +0200

    Right_totolotek

---
+++ Right_totolotek/Right_totolotek/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Right_totolotek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] my_numbers = new int[6];
+            Console.WriteLine("Podaj swoje typy: ");
+            for (int i = 0; i < my_numbers.Length; i++)
+            {
+                my_numbers[i] = Convert.ToInt32(Console.In.ReadLine());
+            }
+            int[] computer_numbers = new int[6];
+            Console.WriteLine("Wylosowano: ");
+            for (int i = 0; i < computer_numbers.Length; i++)
+            {
+                Random rnd = new Random();
+                int number = rnd.Next(1, 43);
+                computer_numbers[i] = number;
+            }
+            for (int i = 0; i < computer_numbers.Length; i++)
+            {
+                Console.Write(computer_numbers[i] + " ");
+            }
+            Console.WriteLine();
+            int counter = 0;
+            for (int i = 0; i < my_numbers.Length; i++)
+            {
+                for (int j = 0; j < computer_numbers.Length; j++)
+                {
+                    if (my_numbers[i] == computer_numbers[j])
+                    {
+                        counter++;
+                    }
+                }
+            }
+            Console.WriteLine($"Trafiles nastepujaca ilosc numerkow: {counter}");
+            
+        }
+    }
+}
