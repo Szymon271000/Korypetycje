df51a296ab4227a1f192ada2bdc817d0c2bef756
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-07 09:34:37 +0200

    Ile_wystapien

---
+++ Ile_wystapien/Ile_wystapien/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ile_wystapien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            Console.Write("Wylosowano: ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Random rnd = new Random();
+                int number = rnd.Next(1, 6);
+                numbers[i] = number;
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+            Console.Write("Po obliczeniach: ");
+            int counter = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == 1)
+                {
+                    counter++;
+                }
+            }
+            Console.Write(counter + " ");
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+    }
+}
