de2c9d8a6ba036d8b35c9d4816e97f1fe59342ee
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-11 23:50:29 +0200

    SamogloskiISpacje

---
+++ SamogloskiISpacje/SamogloskiISpacje/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SamogloskiISpacje
+{
+    class Program
+    {
+        static int Spacje(string tekst)
+        {
+            int counterSp = 0;
+            
+            for (int i = 0; i < tekst.Length; i++)
+            {
+                if (tekst[i] == ' ')
+                {
+                    counterSp++;
+                }
+            }
+            return counterSp;
+        }
+
+        static int Samogloski(string tekst)
+        {
+            int counterSa = 0;
+            for (int i = 0; i < tekst.Length; i++)
+            {
+                if ((tekst[i] == 'A') || (tekst[i] == 'a') || (tekst[i] == 'E') || (tekst[i] == 'e') || (tekst[i] == 'I') || (tekst[i] == 'i') || (tekst[i] == 'O') || (tekst[i] == 'o') || (tekst[i] == 'U') || (tekst[i] == 'u'))
+                {
+                    counterSa++;
+                }
+            }
+            return counterSa;
+        }
+        static void Main(string[] args)
+        {
+            string Input = Console.ReadLine();
+            int resultsa = Samogloski(Input);
+            Console.WriteLine($"Samogloski: {resultsa}");
+            int resultsp = Spacje(Input);
+            Console.WriteLine($"Spacje: {resultsp}");
+        }
+    }
+}
