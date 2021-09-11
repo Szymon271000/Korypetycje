9895937a11953e34833ae5a3e7418e76c1429656
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-11 17:49:23 +0200

    IleSamoglowek

---
+++ Samogloski_w_tekscie/Samogloski_w_tekscie/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Samogloski_w_tekscie
+{
+    class Program
+    {
+        static int IleSamoglowek(string tekst)
+        {
+            int counter = 0;
+            for (int i = 0; i < tekst.Length; i++)
+            {
+                if ((tekst[i] == 'a') || (tekst[i] == 'A') || (tekst[i] == 'E') || (tekst[i] == 'e') || (tekst[i] == 'I') || (tekst[i] == 'i') || (tekst[i] == 'O') || (tekst[i] == 'o') || (tekst[i] == 'U') || (tekst[i] == 'u'))
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+        static void Main(string[] args)
+        {
+            string TekstFromUser = Console.ReadLine();
+            int result = IleSamoglowek(TekstFromUser);
+            Console.WriteLine(result);
+        }
+    }
+}
