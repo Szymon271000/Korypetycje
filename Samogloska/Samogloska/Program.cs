66fb05f70c4aa3ad20af9feb5302bd054496c1ab
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-11 17:23:46 +0200

    Samogloska

---
+++ Samogloska/Samogloska/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Samogloska
+{
+    class Program
+    {
+        static bool Samogloska(string litera)
+        {
+            if ((litera == "A") || (litera == "E") || (litera == "I") || (litera == "O") || (litera == "U") || (litera == "a") || (litera == "e") || (litera == "i") || (litera == "o") || (litera == "u"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string litera = Console.ReadLine();
+            bool result = Samogloska(litera);
+            Console.WriteLine(result);
+        }
+    }
+}
