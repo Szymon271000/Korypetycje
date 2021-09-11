bad70727385541ee03426adc2a06c4da5855a64f
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-12 01:16:23 +0200

    KtoraWieksza

---
+++ KtoraWieksza/KtoraWieksza/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace KtoraWieksza
+{
+    class Program
+    {
+        static int KtoraWieksza(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string Input = Console.ReadLine();
+            string[] Elements = Input.Split(" ");
+            int[] Numbers = new int[Elements.Length];
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Numbers[i] = int.Parse(Elements[i]);
+            }
+            int result = KtoraWieksza(Numbers[0], Numbers[1]);
+            Console.WriteLine(result);
+        }
+    }
+}
