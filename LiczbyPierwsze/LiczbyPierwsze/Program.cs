9dc92a931a9db1f43a71e17acf87aad7612fcbe4
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-11 22:57:08 +0200

    Zeruj

--- LiczbyPierwsze/LiczbyPierwsze/Program.cs
+++ LiczbyPierwsze/LiczbyPierwsze/Program.cs
@@ -4,9 +4,28 @@ namespace LiczbyPierwsze
 {
     class Program
     {
+        static int[] LiczbyPierwsze (int[] Tablica)
+        {
+            for (int i = 0; i < Tablica.Length; i++)
+            {
+                for (int j = 2; j < Tablica[i]; j++)
+                {
+                    if (Tablica [i] % j == 0)
+                    {
+                        return Tablica;
+                    }
+                }
+            }
+            return Tablica;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string Tablica = Console.ReadLine();
+            string[] Numery = Tablica.Split(" ");
+            int[] myInts = Array.ConvertAll(Numery, int.Parse);
+            int[] result = LiczbyPierwsze(myInts);
+            Console.WriteLine(result);
+
         }
     }
 }
--- Zeruj/Zeruj/Program.cs
+++ Zeruj/Zeruj/Program.cs
@@ -4,9 +4,17 @@ namespace Zeruj
 {
     class Program
     {
+        static void Zeruj(int[] Tablica, int rozmiar)
+        {
+            rozmiar = Tablica.Length;
+            for (int i = 0; i < rozmiar; i++)
+            {
+                Tablica[i] = 0;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
