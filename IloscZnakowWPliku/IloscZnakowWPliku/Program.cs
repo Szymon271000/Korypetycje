cc567494301685889b8de9c19f95e9329ed130dc
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-11 23:17:46 +0200

    IloscZnakow

---
+++ IloscZnakowWPliku/IloscZnakowWPliku/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+namespace IloscZnakowWPliku
+{
+    class Program
+    {
+        static int IloscZnakow (string tekst)
+        {
+            int number = 0;
+            for (int i = 0; i < tekst.Length; i++)
+            {
+                number++;
+            }
+            return number;
+        }
+        static void Main(string[] args)
+        {
+            
+            string info = File.ReadAllText(@"C:\Users\huber\OneDrive\Desktop\dane.txt");
+            int ilosc = IloscZnakow(info);
+            Console.WriteLine($"Wynik : {ilosc}");
+        }
+    }
+}
