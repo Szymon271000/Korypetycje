ce8585a8eee625792834eb660d701ab42e5cb0ca
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-11 23:35:06 +0200

    LiczbyWJednejLinii

---
+++ LiczbyWJednejLinii/LiczbyWJednejLinii/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LiczbyWJednejLinii
+{
+    class Program
+    {
+        static int LiczbyWJednejLinii(string dane)
+        {
+            string[] elementy = dane.Split(" ");
+            int[] liczby = new int[elementy.Length];
+            for (int i = 0; i < liczby.Length; i++)
+            {
+                liczby[i] = int.Parse(elementy[i]);
+            }
+            int suma = 0;
+            for (int i = 0; i < liczby.Length; i++)
+            {
+                suma = suma + liczby[i];
+            }
+            return suma;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj liczby: ");
+            string Input = Console.ReadLine();
+            int result = LiczbyWJednejLinii(Input);
+            Console.WriteLine($"Suma: {result}");
+        }
+    }
+}
