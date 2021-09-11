40bc716457eae14caa81c674c53bb23fad300e4e
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-12 00:19:22 +0200

    FunkcjaLosujaca

---
+++ FunkcjaLosujaca/FunkcjaLosujaca/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FunkcjaLosujaca
+{
+    class Program
+    {
+        static int FunkcjaLosujaca(int poczatek, int koniec)
+        {
+            Random rnd = new Random();
+            int Numerpomiedzy = rnd.Next(poczatek, koniec);
+            return Numerpomiedzy;
+        }
+        static void Main(string[] args)
+        {
+            string Input = Console.ReadLine();
+            string[] Elements = Input.Split();
+            int[] Dane = new int[Elements.Length];
+            for (int i = 0; i < Dane.Length; i++)
+            {
+                Dane[i] = int.Parse(Elements[i]);
+            }
+            int result = FunkcjaLosujaca(Dane[0], Dane[1]);
+            Console.WriteLine(result);
+        }
+    }
+}
