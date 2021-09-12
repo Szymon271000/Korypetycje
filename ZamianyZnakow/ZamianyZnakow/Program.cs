e29c9f1b3fcf1ab4cb5a53f3e9567df995b3ece0
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-12 19:07:34 +0200

    ZamianyZnakow

---
+++ ZamianyZnakow/ZamianyZnakow/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ZamianyZnakow
+{
+    class Program
+    {
+        static string ZamianyZnakow(string Test , int indeks)
+        {
+            for (int i = 0; i < Test.Length; i*= indeks)
+            {
+                Test[i] = '*';
+            }
+            return Test;
+        }
+        static void Main(string[] args)
+        {
+            string Test = Console.ReadLine();
+            int number = int.Parse(Console.ReadLine());
+            string result = ZamianyZnakow(Test, number);
+            Console.WriteLine(result);
+        }
+    }
+}
