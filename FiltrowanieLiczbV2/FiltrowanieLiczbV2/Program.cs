c3479430eeb258d22f658f0ef1332467d503ad16
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-12 19:53:31 +0200

    FiltrowanieLiczb

---
+++ FiltrowanieLiczbV2/FiltrowanieLiczbV2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FiltrowanieLiczbV2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string Input = Console.ReadLine();
+            string[] Elements = Input.Split(" ");
+            int[] Numbers = new int[Elements.Length];
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Numbers[i] = int.Parse(Elements[i]);
+            }
+            int min = int.Parse(Console.ReadLine());
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                if (Numbers[i] < min)
+                {
+                    Console.Write(Numbers[i] + " ");
+                }
+            }
+        }
+    }
+}
