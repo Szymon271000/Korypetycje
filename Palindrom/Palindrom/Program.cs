b219e41f2c34164711314be8552d5450a66bad84
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-12 20:04:32 +0200

    Palindrom

---
+++ Palindrom/Palindrom/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Palindrom
+{
+    class Program
+    {
+        static bool Palindrom (string Name)
+        {
+            string reverse = string.Empty;
+
+            for (int i = Name.Length - 1; i >= 0; i--)
+            {
+                reverse += Name[i];
+            }
+
+            if (Name == reverse)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            bool Result = Palindrom("MADAM");
+            Console.WriteLine(Result);
+        }
+    }
+}
