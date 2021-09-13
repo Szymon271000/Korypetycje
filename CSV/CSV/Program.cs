80d233d1d9c3ce256e3762bf2c09a5014956308f
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-13 19:30:14 +0200

    CSV

---
+++ CSV/CSV/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace CSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string info = File.ReadAllText(@"C:\Users\huber\OneDrive\Desktop\Dane2.txt");
+            string[] Users = info.Split("\n");
+            for(int i = 0; i < Users.Length; i++)
+            {
+                string[] user = Users[i].Split(",");
+                Console.WriteLine(user[1]);
+            }
+            
+        }
+    }
+}
