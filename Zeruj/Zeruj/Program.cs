c4aadb19ed0841a08cf5d88892497d7cdf0c6c25
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-13 19:31:25 +0200

    Zeruj

--- Zeruj/Zeruj/Program.cs
+++ Zeruj/Zeruj/Program.cs
@@ -4,9 +4,9 @@ namespace Zeruj
 {
     class Program
     {
-        static void Zeruj(int[] Tablica, int rozmiar)
+        static void Zeruj(int[] Tablica)
         {
-            rozmiar = Tablica.Length;
+            int rozmiar = Tablica.Length;
             for (int i = 0; i < rozmiar; i++)
             {
                 Tablica[i] = 0;
@@ -15,7 +15,7 @@ namespace Zeruj
         static void Main(string[] args)
         {
             int[] Liczby = new int[] { 9, 10, 3, 4, 6 };
-            Zeruj(Liczby, Liczby.Length);
+            Zeruj(Liczby);
             for (int i = 0; i < Liczby.Length; i++)
             {
                 Console.Write(Liczby[i] + " ");
