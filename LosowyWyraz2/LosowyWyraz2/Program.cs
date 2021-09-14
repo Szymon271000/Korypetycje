888bc9486a7c2f3952a8e3657e29a4dd4d31be1a
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-14 12:48:21 +0200

    LosowyWyraz2

---
+++ LosowyWyraz2/LosowyWyraz2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LosowyWyraz2
+{
+    class Program
+    {
+        static string LosowyWyraz2(int Dlugosc)
+        {
+            string Slowo = "abcdefghijklmnopqrstvz";
+            string NewWord = string.Empty;
+            for (int i = 0; i < Dlugosc; i++)
+            {
+                Random rnd = new Random();
+                int month = rnd.Next(Slowo[0], Slowo[21]);
+                char z = (char)month;
+                NewWord = NewWord + z;
+            }
+
+            return NewWord;
+        }
+        static void Main(string[] args)
+        {
+            int Number = int.Parse(Console.ReadLine());
+            string result = LosowyWyraz2(Number);
+            Console.WriteLine(result);
+        }
+    }
+}
