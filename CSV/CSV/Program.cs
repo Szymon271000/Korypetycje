72e905bdbc47a657e24487d46093e16599d51921
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-14 11:03:33 +0200

    CSV

--- CSV/CSV/Program.cs
+++ CSV/CSV/Program.cs
@@ -9,10 +9,30 @@ namespace CSV
         {
             string info = File.ReadAllText(@"C:\Users\huber\OneDrive\Desktop\Dane2.txt");
             string[] Users = info.Split("\n");
+            int counter = 0;
+            int counter2 = 0;
             for(int i = 0; i < Users.Length; i++)
             {
                 string[] user = Users[i].Split(",");
-                Console.WriteLine(user[1]);
+                for (int j = 0; j < user.Length; j++)
+                {
+                    if (user[4] == "Female")
+                    {
+                        counter++;
+                    }
+                }
+                if (user[1].StartsWith("A"))
+                {
+                    Console.WriteLine(user[1]);
+                }
+                if (user[2].StartsWith("T"))
+                {
+                    Console.WriteLine(user[2]);
+                }
+                if (user[3].EndsWith(".pl"))
+                {
+                    counter2++;
+                }
             }
             
         }
