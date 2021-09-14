98fea2b22ca983a4575466d00e5b45e52a0a06cb
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-14 12:31:54 +0200

    LosowyWyraz1

--- LosowyWyraz/LosowyWyraz/Program.cs
+++ LosowyWyraz/LosowyWyraz/Program.cs
@@ -4,9 +4,23 @@ namespace LosowyWyraz
 {
     class Program
     {
+        static string LosowyWyraz(int Liczba)
+        {
+            string Slowo = string.Empty;
+            for (int i = 0; i < Liczba; i++)
+            {
+                Random rnd = new Random();
+                int number = rnd.Next(97, 122);
+                char z = (char)number;
+                Slowo = Slowo + z;
+            }
+            return Slowo;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int Dlugosc = int.Parse(Console.ReadLine());
+            string Result = LosowyWyraz(Dlugosc);
+            Console.WriteLine(Result);
         }
     }
 }
