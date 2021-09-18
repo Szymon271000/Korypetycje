f71f454e054d3794446a99fa0ca95127516cb91e
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-18 21:10:07 +0200

    ImionaZDuzej

---
+++ ImionaZDuzej/ImionaZDuzej/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImionaZDuzej
+{
+    class Program
+    {
+        public static List<string> PrawidloweImiona(List<string> imiona)
+        {
+            List<string> ImionaZDuzej = new List<string>();
+            for (int i = 0; i < imiona.Count; i++)
+            {
+                if (Char.IsUpper(imiona[i], 0))
+                {
+                    ImionaZDuzej.Add(imiona[i]);
+                }
+            }
+            return ImionaZDuzej;
+        }
+        static void Main(string[] args)
+        {
+            List<string> Imiona = new List<string>() { "Jan", "bob", "Krzysztof", "Szymon", "radek", "Andrzej" };
+            List<string> ImionaZDuzej = PrawidloweImiona(Imiona);
+            for (int i = 0; i < ImionaZDuzej.Count; i++)
+            {
+                Console.Write(ImionaZDuzej[i] + " ");
+            }
+        }
+    }
+}
