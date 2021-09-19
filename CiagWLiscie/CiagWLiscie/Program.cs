f3aa0f28f33eb9558863faf6c102e24f357e7eac
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-19 10:23:09 +0200

    CiagWLiscie

---
+++ CiagWLiscie/CiagWLiscie/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CiagWLiscie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbe: ");
+            int Liczba = int.Parse(Console.ReadLine());
+            List<int> ListaLiczb = new List<int>();
+            for (int i = 0; i < Liczba + 1; i++)
+            {
+                ListaLiczb.Add(i);
+            }
+
+            Console.WriteLine("Wynik: ");
+
+            for (int i = 0; i < ListaLiczb.Count; i++)
+            {
+                Console.Write(ListaLiczb[i] + " ");
+            }
+        }
+    }
+}
