1cdfd17c61307f33eab466e7a01783ea4df476e9
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-19 20:13:03 +0200

    SumaElementowListyV2

---
+++ SumaElementowListyV2/SumaElementowListyV2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SumaElementowListyV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            List<int> MojaLista = new List<int>();
+            while (suma < 100 && MojaLista.Count < 11)
+            {
+                Console.Write("Podaj liczbe: ");
+                int n = int.Parse(Console.ReadLine());
+                MojaLista.Add(n);
+                suma = suma + n;
+            }
+            Console.WriteLine("Koniec programu: ");
+            for (int i = 0; i < MojaLista.Count; i++)
+            {
+                Console.Write(MojaLista[i] + " ");
+            }
+        }
+    }
+}
