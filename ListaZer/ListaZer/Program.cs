ccb355c4a6ab62b048b7ad0432642bb2437073bf
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-19 09:50:40 +0200

    ListaZer

---
+++ ListaZer/ListaZer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ListaZer
+{
+    class Program
+    {
+        static List<int> Zera (int Rozmiar)
+        {
+            List<int> ListaZer = new List<int>();
+            for (int i = 0; i < Rozmiar; i++)
+            {
+                ListaZer.Add(0);
+            }
+            return ListaZer;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj rozmiar:");
+            int Liczba = int.Parse(Console.ReadLine());
+            List<int> NewLista = Zera(Liczba);
+            Console.WriteLine("Wynik:");
+            for (int i = 0; i < NewLista.Count; i++)
+            {
+                Console.Write(NewLista[i] + " ");
+            }
+        }
+    }
+}
