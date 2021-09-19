a1913fd3e5ac52de2178fc2c042bac82c8cc6de7
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-19 21:51:00 +0200

    FiltrowanieLiczb

---
+++ FiltrowanieLiczb/FiltrowanieLiczb/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FiltrowanieLiczb
+{
+
+    class Program
+    {
+        static List<int> NowaLista(List<int> ListaStara, int Poczatek, int Koniec)
+        {
+            List<int> NewList = new List<int>();
+            for (int i = 0; i < ListaStara.Count; i++)
+            {
+                if ((ListaStara[i] >= Poczatek) && (ListaStara[i] <= Koniec))
+                {
+                    NewList.Add(ListaStara[i]);
+                }
+            }
+            return NewList;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> MojaLista = new List<int>() { 5, 9, 1, 4, 100, 1, 4, 5, 1, 5, 10, 5 };
+            for (int i = 0; i < MojaLista.Count; i++)
+            {
+                Console.Write(MojaLista[i] + " ");
+            }
+            Console.WriteLine();
+            string Input = Console.ReadLine();
+            string[] Elements = Input.Split(" ");
+            int[] Przedzialy = new int[Elements.Length];
+
+            for (int i = 0; i < Przedzialy.Length; i++)
+            {
+                Przedzialy[i] = int.Parse(Elements[i]);
+            }
+
+            List<int> Result = NowaLista(MojaLista, Przedzialy[0], Przedzialy[1]);
+
+            for (int i = 0; i < Result.Count; i++)
+            {
+                Console.Write(Result[i] + " ");
+            }
+        }
+    }
+}
