be7a8b4d4468ca6b22f0256a79eaad5e9e156dd6
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-19 10:05:06 +0200

    SumaElementowListy

---
+++ SumaElementowListy/SumaElementowListy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SumaElementowListy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ListaLiczb = new List<int>();
+            int sum = 0;
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine($"Podaj element nr {i} :");
+                int Liczba = int.Parse(Console.ReadLine());
+                ListaLiczb.Add(Liczba);
+                sum = sum + Liczba;
+            }
+            Console.Write("Wprowadzono: ");
+            for (int i = 0; i < ListaLiczb.Count; i++)
+            {
+                Console.Write(ListaLiczb[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Suma: {sum}");
+        }
+    }
+}
