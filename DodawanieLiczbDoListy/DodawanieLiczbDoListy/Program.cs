e13050d892137daa5dd0dd7edca3633737168021
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-19 10:13:47 +0200

    DodawanieLiczbdoListy

---
+++ DodawanieLiczbDoListy/DodawanieLiczbDoListy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DodawanieLiczbDoListy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Liczby = new List<int>();
+            Console.WriteLine("Podaj liczbe: ");
+            int Liczba = int.Parse(Console.ReadLine());
+            Liczby.Add(Liczba);
+            while (Liczba != 0)
+            {
+                Console.WriteLine("Podaj liczbe: ");
+                Liczba = int.Parse(Console.ReadLine());
+                Liczby.Add(Liczba);
+            }
+            Console.Write("Koniec programu, wprowadzono: ");
+            for (int i = 0; i < Liczby.Count; i++)
+            {
+                Console.Write(Liczby[i] + " ");
+            }
+        }
+    }
+}
