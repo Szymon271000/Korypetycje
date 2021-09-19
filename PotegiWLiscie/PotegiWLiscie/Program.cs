fae48c0f43cb6c573d8e82f154805d5bcce68431
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-19 19:56:51 +0200

    PotegiWLiscie

---
+++ PotegiWLiscie/PotegiWLiscie/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PotegiWLiscie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj n: ");
+            int n = int.Parse(Console.ReadLine());
+            List<double> MyList = new List<double>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                double IDoKwadratu = Math.Pow(i, 2);
+                MyList.Add(IDoKwadratu);
+            }
+
+            Console.WriteLine("Wynik: ");
+
+            for (int i = 0; i < MyList.Count; i++)
+            {
+                Console.Write(MyList[i] + " ");
+            }
+        }
+    }
+}
