394956ba95fe2bf0a3b7996c78f64e112274c9b9
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-20 19:39:40 +0200

    FibonacciWLiscie

---
+++ FibonacciWLiscie/FibonacciWLiscie/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FibonacciWLiscie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> MojaLista = new List<int>() { 1, 1 };
+            for (int i = 2; i < 10; i++)
+            {
+                MojaLista.Add(MojaLista[i - 2] + MojaLista[i - 1]);
+            }
+
+            for (int i = 0; i < MojaLista.Count; i++)
+            {
+                Console.WriteLine(MojaLista[i] + " ");
+            }
+        }
+    }
+}
