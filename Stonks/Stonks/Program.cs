f4667a72fdcfc0c984c6c5ca7c9023aefb45c989
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-20 19:41:07 +0200

    Stonks

---
+++ Stonks/Stonks/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stonks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbe: ");
+            int n = int.Parse(Console.ReadLine());
+            List<int> MojaLista = new List<int>();
+            int min = int.MinValue;
+            while (n > min)
+            {
+                MojaLista.Add(n);
+                min = n;
+                Console.WriteLine("Podaj liczbe: ");
+                n = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
