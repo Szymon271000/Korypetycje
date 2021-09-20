6bbab82b7913491ae1500c2a1d234a796b5ef194
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-20 09:55:41 +0200

    LosoweZdanie

---
+++ LosoweZdanie/LosoweZdanie/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LosoweZdanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> MojaLista = new List<string>();
+            string NewSentence = string.Empty;
+
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine("Podaj slowo: ");
+                string slowo = Console.ReadLine();
+                MojaLista.Add(slowo);
+            }
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                int slowo = rnd.Next(0, MojaLista.Count);
+                NewSentence = NewSentence + MojaLista[slowo] + " ";
+            }
+
+            Console.WriteLine(NewSentence);
+        }
+    }
+}
