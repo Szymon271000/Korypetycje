7e928714a2ec77d82e22d54efea207e7a7184585
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-20 19:40:06 +0200

    Inicjaly

---
+++ Inicjaly/Inicjaly/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inicjaly
+{
+    class Program
+    {
+        static List<string> LiteryImion (List<string> Imiona)
+        {
+            List<string> ListZDuzej = new List<string>();
+            for (int i = 0; i < Imiona.Count; i++)
+            {
+                ListZDuzej.Add(Imiona[i][0] + "");
+            }
+            return ListZDuzej;
+        }
+        static void Main(string[] args)
+        {
+            List<string> Imiona = new List<string>() { "Ada", "Ola", "Ania", "Tomasz", "Janusz" };
+            List<string> Result = LiteryImion(Imiona);
+            for (int i = 0; i < Result.Count; i++)
+            {
+                Console.WriteLine(Result[i]);
+            }
+        }
+    }
+}
