f3d3d1949183f42985c6787349ddfbb7cadd8a36
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-27 17:42:30 +0200

    Stozek

---
+++ Stozek/Stozek/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Stozek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stozek S1 = new Stozek(promien: 3, wysokosc: 5);
+            Console.WriteLine(S1);
+        }
+    }
+}
---
+++ Stozek/Stozek/Stozek.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stozek
+{
+    class Stozek
+    {
+        public int Promien;
+        public int Wysokosc;
+
+        public Stozek()
+        {
+            Promien = 4;
+            Wysokosc = 7;
+        }
+
+        public Stozek(int promien, int wysokosc)
+        {
+            Promien = promien;
+            Wysokosc = wysokosc;
+        }
+
+        public double PolePodstawowe { get => Math.PI * Promien * Promien; }
+
+        public double Objetosc { get => (PolePodstawowe * Wysokosc) / 3; }
+
+        public double L { get => Math.Sqrt((Promien * Promien) + (Wysokosc * Wysokosc)); }
+
+        public double PoleBoczne { get => Math.PI * Promien * L; }
+
+        public double PoleCalkowite { get => PolePodstawowe + PoleBoczne; }
+
+        public override string ToString()
+        {
+            return $"Wzorek o promieniu {Promien} i o wysokosci {Wysokosc} ma pole podstawowe: {PolePodstawowe} , pole boczne: {PoleBoczne} , pole calkowite {PoleCalkowite} , tworzacza {L} i objetosc {Objetosc}";
+        }
+    }
+}
