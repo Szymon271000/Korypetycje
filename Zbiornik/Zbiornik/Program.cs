8d436d58869c54735aaee89e72ab6c383a882951
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-09-27 16:40:30 +0200

    Zbiornik

---
+++ Zbiornik/Zbiornik/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Zbiornik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zbiornik Z1 = new Zbiornik(numerZbiornika: 1, pojemnosc: 200, stanWody: 0);
+            Console.WriteLine(Z1);
+            Z1.Dolej = 100;
+            Console.WriteLine(Z1);
+            Z1.Odlej = 50;
+            Console.WriteLine(Z1);
+            Zbiornik Z2 = new Zbiornik(numerZbiornika: 2, pojemnosc: 1000, stanWody: 500);
+            Console.WriteLine(Z2);
+        }
+    }
+}
---
+++ Zbiornik/Zbiornik/Zbiornik.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zbiornik
+{
+    class Zbiornik
+    {
+        public int NumerZbiornika;
+        public int Pojemnosc;
+        public int StanWody;
+
+        public Zbiornik()
+        {
+            StanWody = 0;
+        }
+
+        public Zbiornik(int numerZbiornika, int pojemnosc, int stanWody)
+        {
+            NumerZbiornika = numerZbiornika;
+            Pojemnosc = pojemnosc;
+            StanWody = stanWody;
+        }
+
+        public int Dolej
+        {
+            set
+            {
+                if (value >= 0 && value <= Pojemnosc)
+                {
+                     StanWody += value; 
+                }
+            }
+        }
+        public int Odlej
+        {
+            set
+            {
+                if (value >= 0 && value <= Pojemnosc && StanWody > 0)
+                {
+                    StanWody -= value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Zbiornik {NumerZbiornika} z pojemnoscia {Pojemnosc} stan wody {StanWody}";
+        }
+    }
+}
