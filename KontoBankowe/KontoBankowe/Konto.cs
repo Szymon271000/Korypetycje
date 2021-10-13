2e9550e408d655d270c6b59da8598a28c68749b7
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-10-13 17:49:03 +0200

    KontoBankowe

---
+++ KontoBankowe/KontoBankowe/Konto.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KontoBankowe
+{
+    class Konto
+    {
+        private float stankonta;
+        private int debet;
+        private float oprocentowanie;
+        private string pin;
+
+        public Konto(float oprocentowanie)
+        {
+            this.oprocentowanie = oprocentowanie;
+        }
+
+
+        public void Wyplac(int kwota)
+        {
+            if (kwota <= stankonta)
+            {
+                float roznica = stankonta - kwota;
+                stankonta = roznica;
+            }
+            else
+            {
+                Console.WriteLine("Operacja niemozliwa");
+            }
+        }
+        public void Wplac(float kwota)
+        {
+            float suma = kwota + stankonta;
+            stankonta = suma;
+        }
+
+        public float SetDebet(float kwota)
+        {
+            stankonta = kwota;
+            return stankonta;
+        }
+
+        public void SetPin(string Newpin)
+        {
+            if (Newpin.Length != 4)
+            {
+                Console.WriteLine("Podany numer ma bledny format");
+            }
+            else
+            {
+                pin = Newpin;
+            }
+        }
+
+        public void Kapitalizacja()
+        {
+            // ask how to use SetDebet(stankonta)
+            float NowyStanKonta = stankonta - 500 + ((stankonta -500) *(oprocentowanie / 12)); //SetDebet(stankonta)// 
+            stankonta = NowyStanKonta;
+        }
+
+        public bool SprawdzPin(int pin)
+        {
+            string NewPin = pin.ToString();
+            if (NewPin == this.pin)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void PozazStan()
+        {
+            Console.WriteLine($"{stankonta}");
+        }
+    }
+}
---
+++ KontoBankowe/KontoBankowe/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace KontoBankowe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Konto k = new Konto(0.05f);
+            k.SetDebet(500);
+            k.SetPin("1234");
+            Console.WriteLine("Podaj PIN: ");
+            int odp = int.Parse(Console.ReadLine());
+            if (k.SprawdzPin(odp))
+            {
+                k.PozazStan();
+                k.Wyplac(300);
+                k.PozazStan();
+                k.Wyplac(500);
+                k.PozazStan();
+                k.Wplac(1000);
+                k.PozazStan();
+            }
+            k.Kapitalizacja();
+            k.PozazStan();
+            k.SetPin("123");
+            
+        }
+    }
+}
