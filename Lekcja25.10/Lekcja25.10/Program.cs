0df3094c5742c3db43d45dca1944a43cc9d02598
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-10-26 09:28:26 +0200

    Lekcja25.10

---
+++ Lekcja25.10/Lekcja25.10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lekcja25._10
+{
+    class Program
+    {
+        static int pobierzInt()
+        {
+            Console.WriteLine("Podaj liczbe");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            bool koniec = false;
+            int i = 0;
+            float suma = 0;
+            while(koniec == false)
+            {
+                try
+                {
+                    int pobranaLiczba = pobierzInt();
+                    i++;
+                    suma += pobranaLiczba;
+                }
+                catch(FormatException)
+                {
+                    koniec = true;
+                }
+            }
+            if(i != 0)
+            {
+                Console.WriteLine(suma / i);
+            }
+        }
+    }
+}
