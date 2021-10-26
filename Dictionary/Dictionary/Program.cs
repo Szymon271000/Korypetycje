a5baee2717167332383c05342b599172e3bc3c91
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-10-26 09:28:03 +0200

    Dictionary

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryLekcja
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> slownik = new Dictionary<string, string>();
+            slownik.Add("dog", "pies");
+            slownik.Add("cat", "kot");
+            slownik.Add("mirror", "lustro");
+
+            Console.WriteLine(slownik["dog"]);
+            slownik["dog"] = "piesek"; //zmiana wartosci pod kluczem dog
+            Console.WriteLine(slownik["dog"]);
+
+            Console.WriteLine();
+            foreach (var item in slownik)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+
+            Dictionary<string, Punkt> punkty = new Dictionary<string, Punkt>();
+            punkty.Add("bohater", new Punkt(0, 0));
+            punkty.Add("zamek", new Punkt(10, 5));
+            punkty.Add("smok", new Punkt(50, 11));
+
+            Punkt p = punkty["bohater"];
+            Console.WriteLine(p.X + " " + p.X);
+        }
+    }
+}
---
+++ Dictionary/Dictionary/Punkt.cs
@@ -0,0 +1,19 @@
+﻿namespace DictionaryLekcja
+{
+        class Punkt
+        {
+            private int x;
+            private int y;
+
+        public Punkt(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+
+
+    }
+}
