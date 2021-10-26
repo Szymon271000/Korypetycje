32bc44524aa9ebdb77a24a32c41f95a3713868dd
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-10-26 09:28:47 +0200

    WczytywanieZWyjatkami

---
+++ WczytywanieZWyjatkami/WczytywanieZWyjatkami/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace WczytywanieZWyjatkami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            int i = 0;
+            try
+            {
+                Console.Write("Podaj liczbe: ");
+                string Input = Console.ReadLine();
+                int number1 = int.Parse(Input);
+                suma += number1;
+
+                while (true)
+                {
+                    Console.Write("Podaj liczbe: ");
+                    Input = Console.ReadLine();
+                    int number = int.Parse(Input);
+                    suma = suma + number;
+                    i++;
+                }
+               
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Zly format");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Liczba za duza");
+            }
+
+            if (i != 0)
+            {
+                int srednia = suma / i;
+                Console.WriteLine(srednia);
+            }
+            
+
+        }
+    }
+}
