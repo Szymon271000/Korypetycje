341af26035ed90446b7400f6551f82cf46ac0550
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-10-30 00:12:26 +0200

    Lekcja28.10Prototyp

---
+++ Lekcja28.10Prototyp/Lekcja28.10Prototyp/Character.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lekcja28._10Prototyp
+{
+    abstract class Character
+    {
+        private int hp;
+
+        public Character(int hp)
+        {
+            this.Hp = hp;
+        }
+
+        public int Hp { get => hp; set => hp = value; }
+
+        public abstract void PrzyjmiCios(int silaCiosu);
+
+        public abstract void ZadajCios(Character character);
+
+
+        public override string ToString()
+        {
+            return $"Character ma {hp}";
+        }
+    }
+}
---
+++ Lekcja28.10Prototyp/Lekcja28.10Prototyp/Knight.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lekcja28._10Prototyp
+{
+    class Knight : Character
+    {
+        public Knight() : base(100)
+        {
+            
+        }
+        public override void PrzyjmiCios(int silaCiosu)
+        {
+            Hp = Hp - silaCiosu;
+        }
+
+        public override void ZadajCios(Character character)
+        {
+            character.PrzyjmiCios(15);
+        }
+    }
+}
