1fcdc7555b528a376ab131a24802e022b45638a5
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-11-01 19:33:16 +0100

    Lekcja01.11Prototyp

---
+++ Lekcja01.11/Lekcja01.11/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lekcja01._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Devil devil1 = new Devil("Red", 100, 10);
+            Devil devil2 = new Devil("Black", 150, 20);
+            Demon demon1 = new Demon("Neder", 90);
+
+            Spawn spawn1 = new Spawn();
+            spawn1.Add(devil1);
+            spawn1.Add(devil2);
+            spawn1.Add(demon1);
+            Monster m1 = spawn1.create("Red");
+            Console.WriteLine(m1);
+
+            int newHp = devil1.Hp * 2;
+            devil1.Hp = newHp;
+            Monster m2 = spawn1.create("Red");
+            Console.WriteLine(m2);
+            Console.WriteLine();
+            Console.WriteLine(m1);
+
+            Dictionary<string, float> miasta = new Dictionary<string, float>();
+            miasta.Add("Warszawa", 17.67f);
+            miasta.Add("Kraków", 19.32f);
+            miasta.Add("Wroclaw", 22.31f);
+            miasta.Add("Katowice", 17.77f);
+            miasta.Add("Rzeszow", 24.16f);
+            miasta.Add("Poznan", 19.86f);
+
+            foreach (var miasto in miasta)
+            {
+                if (miasto.Value > 20)
+                {
+                    Console.WriteLine(miasto);
+                }
+            }
+
+        }
+    }
+}
---
+++ Lekcja01.11/Lekcja01.11/Spawn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lekcja01._11
+{
+    class Spawn
+    {
+        List<Monster> monsters = new List<Monster>();
+
+        public void Add (Monster m)
+        {
+            monsters.Add(m);
+        }
+
+        public Monster create(string imie)
+        {
+            for (int i = 0; i < monsters.Count; i++)
+            {
+                if (imie == monsters[i].Imie)
+                {
+                    return monsters[i].Clone();
+                }
+            }
+
+            throw new KeyNotFoundException();
+            
+        }
+    }
+}
