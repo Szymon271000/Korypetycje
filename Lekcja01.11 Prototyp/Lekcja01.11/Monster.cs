d5f3f6c27a2d904ca5dbf7add8a87c99018ce9b6
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-11-03 18:37:17 +0100

    Lekcja01.11Prototyp

---
+++ Lekcja01.11 Prototyp/Lekcja01.11/Monster.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lekcja01._11
+{
+    class Monster
+    {
+        private string imie;
+        private int hp;
+
+        public Monster(string imie, int hp)
+        {
+            this.Imie = imie;
+            this.Hp = hp;
+        }
+
+        public string Imie { get => imie; set => imie = value; }
+        public int Hp { get => hp; set => hp = value; }
+
+        public Monster Clone()
+        {
+            return (Monster)MemberwiseClone();
+        }
+
+        public override string ToString()
+        {
+            return $"Monster {imie} ma {hp}";
+        }
+    }
+}
