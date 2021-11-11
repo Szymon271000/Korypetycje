0fba0cbe3dda9f7965871cd60e31cdc8f9673b27
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-11-11 17:43:59 +0100

    Lekcja08.11

---
+++ Lekcja08.11(2)/Lekcja08.11(2)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lekcja08._11_2_
+{
+    class Program
+    {
+        static void Pokaz<MojTyp>(List<MojTyp> lista)
+        {
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<string> slowa = new List<string>
+            {
+                "ala", "ma", "kota"
+            };
+            Pokaz(slowa);
+
+            List<int> liczby = new List<int>() { 10, 20, 30, 40 };
+            Pokaz(liczby);
+
+            Stos<int> stos = new Stos<int>();
+            stos.Push(10);
+            stos.Push(20);
+            stos.Push(30);
+
+            Console.WriteLine(stos.Pop());
+        }
+    }
+}
---
+++ Lekcja08.11(2)/Lekcja08.11(2)/Stos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lekcja08._11_2_
+{
+    class Stos<T> //klasa generyczna
+    {
+        private List<T> lista = new List<T>();
+
+        public void Push(T element)
+        {
+            lista.Add(element);
+        }
+
+        public T Peek()
+        {
+            return lista[lista.Count-1];
+        }
+
+        public T Pop()
+        {
+            T el = lista[lista.Count-1];
+            lista.RemoveAt(lista.Count - 1);
+            return el;
+        }
+    }
+}
