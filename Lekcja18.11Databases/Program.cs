e92725b224fc01309a10b6b47a3d872cb784fa7c
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-11-18 22:09:22 +0100

    Lekcja18.11Databases

---
+++ Lekcja18.11Databases/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lekcja18._11Databases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
