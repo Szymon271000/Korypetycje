1beda9ee792091b39316d1057a124a4cd3999b0d
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-11-18 20:57:17 +0100

    PunktyZLinq

---
+++ PunktyZLINQ/PunktyZLINQ/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PunktyZLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            
+
+            Punkt[] punkts = new Punkt[100];
+
+
+            for (int i = 0; i < punkts.Length; i++)
+            {
+                int number = rnd.Next(-20, 21);
+                int number2 = rnd.Next(-20, 21);
+                punkts[i] = new Punkt(number, number2);
+            }
+
+            List<Punkt> punkty = new List<Punkt>();
+            for (int i = 0; i < punkts.Length; i++)
+            {
+                punkty.Add(punkts[i]);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < punkty.Count; i++)
+            {
+                Console.WriteLine(punkty[i]);
+            }
+
+            int p1 = punkty.Count(x => x.X > 0 && x.Y > 0); // spytac czy tylko >0 czy >=0
+            int p2 = punkty.Count(x => x.X < 0 && x.Y > 0);
+            int p3 = punkty.Count(x => x.X < 0 && x.Y < 0);
+            int p4 = punkty.Count(x => x.X > 0 && x.Y < 0);
+
+            List<Punkt> dwacyfry = punkty.FindAll(x => x.X >= 10 && x.Y >= 10);
+
+            bool rowna20 = punkty.Any(x => x.X == -20 && x.Y < 0);
+
+
+            List<string> xs = punkty.Select(x => x.X.ToString()).ToList(); 
+            List<string> ys = punkty.Select(x => x.Y.ToString()).ToList();
+
+            for (int i = 0; i < xs.Count; i++)
+            {
+                Console.WriteLine($"{xs[i]}, {ys[i]}");
+            }
+        }
+    }
+}
---
+++ PunktyZLINQ/PunktyZLINQ/Punkt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PunktyZLINQ
+{
+    class Punkt
+    {
+        private int x;
+        private int y;
+
+        public Punkt(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+
+        public override string ToString()
+        {
+            return $"Punkt ma x: {X} i y: {Y}";
+        }
+    }
+}
