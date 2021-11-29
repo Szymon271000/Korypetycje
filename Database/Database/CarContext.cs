a129a531e14e957db0b70a9d0929d9ebc25007ed
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-11-29 21:09:20 +0100

    Database

---
+++ Database/Database/CarContext.cs
@@ -0,0 +1,18 @@
+﻿using Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+
+namespace Database
+{
+        public class CarContext : DbContext
+        {
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"Server=localhost\SQLEXPRESS02;Database=master;Trusted_Connection=True;");
+            }
+
+            public DbSet<Car> Cars { get; set; }
+        }
+    }
+
---
+++ Database/Database/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarContext carcontex1 = new CarContext();
+            Car c1 = new Car
+            {
+                Marka = "Audi",
+                Model = "A4"
+            };
+
+            for (int i = 0; i < 5; i++)
+            {
+               
+                string Input = Console.ReadLine();
+                string[] carsdata = Input.Split(" ");
+                Car c = new Car();
+                c.Marka = carsdata[0];
+                c.Model = carsdata[1];
+                carcontex1.Add(c);
+            }
+            carcontex1.SaveChanges();
+        }
+    }
+}
