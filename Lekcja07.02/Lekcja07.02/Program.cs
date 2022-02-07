438f8876ac56f2e98c1542179ae36c2dba666640
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-02-07 20:54:38 +0100

    Lekcja07.02Action,Func

---
+++ Lekcja07.02/Lekcja07.02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lekcja07._02
+{
+    class Program
+    {
+        static void Hello()
+        {
+            Console.WriteLine("Hello world!");
+        }
+
+        static void Potega(int a, int b)
+        {
+            Console.WriteLine(Math.Pow(a, b));
+        }
+
+        static int Dodawanie(double x, double y)
+        {
+            return (int)(x + y);
+        }
+
+        static int RzutKostka()
+        {
+            Random rand = new Random();
+            return rand.Next(1, 7);
+        }
+
+        static void Main(string[] args)
+        {
+            Action x = Hello;
+            x();
+
+            Action<int, int> a = Potega;
+            a(2, 5);
+
+            Func<double, double, int> funkcja = Dodawanie; //<typ argumentu 1, typ argumentu 2, typ zwracany>
+            Console.WriteLine(funkcja(10, 20));
+
+            Func<int> func = RzutKostka;
+            Console.WriteLine(func());
+        }
+    }
+}
