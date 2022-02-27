34fc521b28fe7fe8a01da6b9049e3697fdcb5a51
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-02-27 17:19:00 +0100

    Zadania

---
+++ Zadania/Zadania/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Zadania
+{
+    public static class StringExtension
+    {
+        public static int WordCounter(this string str)
+        {
+            int counter = 0;
+            string[] userText = str.Split(' ');
+            foreach (var item in userText)
+            {
+                counter += item.Length;
+            }
+            return counter;
+        }
+    }
+
+    class Program
+    {
+        static int Add(int a, int b)
+        {
+            while (a > 0)
+            {
+                b++;
+                a--;
+            }
+
+            while (a < 0)
+            {
+                b--;
+                a++;
+            }
+
+            return b;
+        }
+
+        static void Main(string[] args)
+        {
+            //string Text = Console.ReadLine();
+            //int TotalWords = 0;
+            //TotalWords = Text.WordCounter();
+            //Console.WriteLine($"Total wrods: {TotalWords}");
+            int FirstNumber = int.Parse(Console.ReadLine());
+            int SecondNumber = int.Parse(Console.ReadLine());
+            var sum = Add(FirstNumber, SecondNumber);
+            Console.WriteLine(sum);
+        }
+    }
+}
